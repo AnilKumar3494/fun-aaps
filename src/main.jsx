bb52ccf219320fe7a165aae1ad0be1e0c4e6c5dd
Anil <karapaanilkumar005@gmail.com>
2024-02-12 23:40:23 -0500

    test but not req

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Valentine from "./components/valentine/Valentine.jsx";
+import { createBrowserRouter } from "react-router-dom";
+
+//for deployment
+const router = createBrowserRouter({
+  path: "/fun-aaps/",
+  element: <App />,
+  children: [
+    {
+      path: "/fun-aaps/",
+      element: <Valentine />,
+    },
+  ],
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
